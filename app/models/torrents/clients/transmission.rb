f7d5a91a01218648ea9f087f88b968b7981c4cd5
oniksfly <oniksfly@gmail.com>
2019-06-14 22:51:13 +0300

    Check transmission connection

--- app/models/torrents/clients/transmission.rb
+++ app/models/torrents/clients/transmission.rb
@@ -4,13 +4,33 @@ module Torrents
   module Clients
     class Transmission < Client
 
+      attr_accessor :rpc
+
       def type_name
         'Transmission'
       end
 
-      def list_all_torrents
-        torrents = Transmission::Model::Torrent.all
-        puts torrents.inspect
+      def connection
+        if @rpc.present?
+          @rpc
+        else
+          params = { host: host, port: port, ssl: false }
+          params[:path] = path if path.present?
+          if username.present?
+            params[:credentials] = { username: username }
+            params[:credentials][:password] = password if password.present?
+          end
+
+          @rpc = ::Transmission::RPC.new(params)
+        end
+      end
+
+      def available?
+        begin
+          connection.get_session.try(:[], 'version').present?
+        rescue => exception
+          false
+        end
       end
     end
   end
