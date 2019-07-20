f8b794b94ad284a8e63fc72d05aff58078e2ed6f
Ilya Krigouzov <oniksfly@gmail.com>
2019-07-20 23:17:35 +0300

    Token authorisation on API

--- app/services/network_service.rb
+++ app/services/network_service.rb
@@ -2,9 +2,11 @@ class NetworkService
   include HTTParty
   base_uri Rails.configuration.remote_backend_url
 
-  def initialize
-    # TODO: Set token value
-    @token = nil
+  def initialize(token)
+    @token = token
+
+    self.class.headers 'Authorization-Entity' =>  'local_agent'
+    self.class.headers 'Authorization' => @token
   end
 
   def user_torrents_list
